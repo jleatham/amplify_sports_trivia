51b9cd1523d06c73443bfdd6d39da42647cbe8a2
EC2 Default User <ec2-user@ip-172-31-11-105.ec2.internal>
2020-05-26 21:30:57 +0000

    added analytics

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 /* Location 6 */
 import { withAuthenticator } from 'aws-amplify-react';
-
+import { Analytics, Auth } from 'aws-amplify';
 import Game from '../Game';
 import './styles/index.css';
 
@@ -14,3 +14,39 @@ const App = () => (
 /* Location 7 */
 export default withAuthenticator(App, {includeGreetings: true});
 
+
+
+//added analytics
+Analytics.autoTrack('session', {
+    enable: true,
+    provider: 'AWSPinpoint'
+});
+
+Analytics.autoTrack('pageView', {
+    enable: true,
+    eventName: 'pageView',
+    type: 'SPA',
+    provider: 'AWSPinpoint',
+    getUrl: () => {
+        return window.location.origin + window.location.pathname;
+    }
+});
+
+const mappedobjects = f => obj =>
+  Object.keys(obj).reduce((acc, key) => ({ ...acc, [key]: f(obj[key]) }), {});
+const Arrayofourstrings = value => [`${value}`];
+const mapArrayofourstrings = mappedobjects(Arrayofourstrings);
+
+async function trackUserIdforPinpoint() {
+    const { attributes } = await Auth.currentAuthenticatedUser();
+    const userAttributes = mapArrayofourstrings(attributes);
+    Analytics.updateEndpoint({
+      address: attributes.email,      
+      channelType: 'EMAIL',      
+      optOut: 'NONE',      
+      userId: attributes.sub,      
+      userAttributes,    
+    });
+  } 
+
+trackUserIdforPinpoint();
